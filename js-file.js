d7af978447da3105158af001207bd58caff6cf11
Nick <nicolasshanks10@gmail.com>
2022-04-18 12:10:54 -0400

    Add js-file.js and main.html and style.css

---
+++ js-file.js
@@ -0,0 +1,27 @@
+function add(a,b){
+    let sum = 0
+    a + b;
+    return sum;
+}
+
+function sub(a,b){
+    let prod = 0
+    a - b;
+    return prod;
+}
+
+function mult(a,b){
+    let prod = 0
+    a * b;
+    return prod;
+}
+
+function divide(a,b){
+    let prod = 0
+    a / b;
+    return prod;
+}
+
+function operate(){
+    
+}
