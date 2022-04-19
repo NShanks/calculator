02d886bd64ae956060bc03bab0cde605b199fea4
Nick <nicolasshanks10@gmail.com>
2022-04-18 23:27:02 -0400

    js-file.js and main.html and style.css

--- js-file.js
+++ js-file.js
@@ -1,27 +1,98 @@
-function add(a,b){
-    let sum = 0
-    a + b;
-    return sum;
+let entry = document.getElementById("display")
+let dummy = document.getElementById("display2")
+
+entry.textContent = '';
+dummy.textContent = '';
+let firstNumber = '';
+let secondNumber = '';
+let operator;
+
+function keyPress(e){
+    entry.textContent += e;
+    dummy.textContent = '';
+    checkNums();
 }
 
-function sub(a,b){
-    let prod = 0
-    a - b;
-    return prod;
+function wipe() {
+    entry.textContent = '';
+    dummy.textContent = '';
+    firstNumber = '';
+    secondNumber = '';
+    console.log('wipe');
+    checkNums();
 }
 
-function mult(a,b){
-    let prod = 0
-    a * b;
-    return prod;
-}
+function add(){
+    // if (firstNumber != '' && entry != ''){
+    //     entry = parseInt(firstNumber) + parseInt(entry);
+    // }
 
-function divide(a,b){
-    let prod = 0
-    a / b;
-    return prod;
-}
-
-function operate(){
+    firstNumber = entry.textContent;
+    dummy.textContent = entry.textContent;
+    entry.textContent = '';
+    operator = 'addition';
     
+    
+    // if (firstNumber != '' && secondNumber != '')
+    // {
+    //     entry.textContent = firstNumber.textContent;
+    //     dummy.textContent = firstNumber.textContent;
+    //     console.log(firstNumber.textContent)
+    // }
+    checkNums();
+}
+
+function sub(){
+    firstNumber = entry.textContent;
+    dummy.textContent = entry.textContent;
+    entry.textContent = '';
+    operator = 'subtraction';
+}
+
+function mult(){
+    firstNumber = entry.textContent;
+    dummy.textContent = entry.textContent;
+    entry.textContent = '';
+    operator = 'multiplication';
+}
+
+function divide(){
+    firstNumber = entry.textContent;
+    dummy.textContent = entry.textContent;
+    entry.textContent = '';
+    operator = 'division';
+}
+
+
+
+function equals(){
+    if (operator == 'addition'){
+        if (secondNumber ==''){secondNumber = entry.textContent;}//left off here trying to get the equals button to work after hitting another operator button 
+        entry.textContent = parseInt(firstNumber) + parseInt(secondNumber);
+        firstNumber = entry.textContent;
+        checkNums();
+    }
+    if (operator == 'subtraction'){
+        secondNumber = entry.textContent;
+        entry.textContent = parseInt(firstNumber) - parseInt(secondNumber);
+    }
+    if (operator == 'multiplication'){
+        secondNumber = entry.textContent;
+        entry.textContent = parseInt(firstNumber) * parseInt(secondNumber);
+    }
+    if (operator == 'division'){
+        secondNumber = entry.textContent;
+        entry.textContent = parseInt(firstNumber) / parseInt(secondNumber);
+    }
+    dummy.textContent = '';
+}
+
+
+function checkNums()
+{
+    console.log('-----New Button Below-----');
+    console.log('Entry: ' + entry.textContent);
+    console.log('First: ' + firstNumber);
+    console.log('Second: ' + secondNumber);
+    console.log('Dummy: ' + dummy.textContent);
 }
